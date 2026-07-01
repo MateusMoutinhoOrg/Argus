95de2f9acceb1d8fc461106939e956ba276a06b4
matesmoutinho <matesmoutinho01@gmail.com>
2026-07-01 17:45:04 -0300

    feat: update Generate method signature to accept GenerationProps and add struct definitions for sample usage

--- pkg/Argus/generate.go
+++ pkg/Argus/generate.go
@@ -1,6 +1,17 @@
 package Argus
 
-func (l Lib) Generate() int {
+type Errors string {
+	MissingFlag string 
+	MissingArg string 
+	UnknowAction string 
+	UnknowArg string 
+}
+type GenerationProps string{
+
+
+}
+
+func (l Lib) Generate(GenerationProps props) int {
 
 	//return 0 on fail
 	return 0
--- samples/calc.go
+++ samples/calc.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/MateusMoutinhoOrg/Argus/adapters/native"
 	"github.com/MateusMoutinhoOrg/Argus/pkg/Argus"
 )
 
+type FirstActionEntries struct {
+	a string `arg: "next"`
+	b string `arg: "next"`
+}
+
+func sum(entries FirstActionEntries) {
+	fmt.Println("sum is : ", entries.a+entries.b)
+}
+
 func main() {
 
 	argus := Argus.New(native.New())
-	argus.Generate()
+
+	props := Argus.GenerationProps{}
+
+	argus.Generate(props)
 
 }
